a2244d76b63e45553a65ef555a77a59d2e424665
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-03-25 16:20:33 -0500

    moves routes to routes/index.js

--- server.js
+++ server.js
@@ -1,55 +1,11 @@
 const express = require("express");
+const routes = require("./routes");
 const app = express();
 
-// mock db data for users and posts
-const users = [
-  {
-    username: "user 1",
-  },
-  {
-    username: "user 2",
-  },
-];
+// links routes folder
+app.use(routes);
 
-const posts = [
-  {
-    post_title: "awesome post",
-    post_body: "This is an awesome post",
-    author: "user 1",
-  },
-  {
-    post_title: "awesome post number 2",
-    post_body: "This is another awesome post",
-    author: "user 2",
-  },
-];
-
-const comments = [
-  {
-    comment_body: "This is really cool!",
-    user_id: 1,
-    post_id: 2,
-  },
-  {
-    comment_body: "Nice!",
-    user_id: 2,
-    post_id: 1,
-  },
-];
-
-// get route for all users
-app.get("/api/users", (req, res) => {
-  res.json(users);
-});
-
-// get route for all posts
-app.get("/api/posts", (req, res) => {
-  res.json(posts);
-});
-
-// get route for all posts
-app.get("/api/comments", (req, res) => {
-  res.json(comments);
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(3001);
