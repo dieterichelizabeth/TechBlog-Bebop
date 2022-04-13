d3c7014e56e19264367b4a0f0ae265034496786e
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-04-13 02:03:24 -0500

    Adds user id to post request

--- public/javascript/newPost.js
+++ public/javascript/newPost.js
@@ -2,9 +2,10 @@ console.log("now listeneing");
 async function loginFormHandler(event) {
   event.preventDefault();
 
-  const user_id = session.user_id;
-  const title = document.querySelector("#email-login").value.trim();
-  const body = document.querySelector("#password-login").value.trim();
+  var idEl = document.querySelector(".post_ref");
+  const user_id = idEl.innerHTML;
+  const title = document.querySelector("#postTitle").value.trim();
+  const body = document.querySelector("#postBody").value.trim();
 
   if (title && body) {
     const response = await fetch("/api/posts", {
