0f88859fa9fc97ff82e602002060cbb1f5977046
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-04-14 14:23:53 -0500

    Adds save button

--- public/javascript/editComment.js
+++ public/javascript/editComment.js
@@ -1,20 +1,38 @@
 async function editCommentHandler(event) {
   console.log("editing comment");
+  const buttonDiv = event.target.parentNode;
+  const commentId = event.target.parentNode.id;
 
-  // create the text area, add the save button, hide the edit and delete
+  // get the previous comment
   const commentEditor =
     event.target.parentNode.parentNode.parentNode.childNodes[1];
   const previousComment = commentEditor.innerHTML;
-  console.log(commentEditor);
 
+  // create the text area
   const textarea = document.createElement("textarea");
   textarea.classList.add("w100");
   textarea.innerHTML = previousComment;
-
   commentEditor.replaceWith(textarea);
+
+  // disable the edit button
+  document.getElementById("editComment").disabled = true;
+
+  // create the save button
+  const saveButton = document.createElement("button");
+  saveButton.setAttribute("id", "saveComment");
+  saveButton.classList.add("editbutton");
+  saveButton.innerHTML = "Save";
+  buttonDiv.appendChild(saveButton);
   // pass the save button as an argument to the next function
+  saveCommentHandler();
 }
 
+async function saveCommentHandler(event) {
+  const saveButton = document.getElementById("saveComment");
+  saveButton.addEventListener("click", () => {
+    console.log("Saving comment");
+  });
+}
 // create a function to handle the routes to update the comment w/ event listener
 // route will be similar to the new comment route
 // re-display the single-post page
