9bade684ae75ab42c6fa66079bf3a16252c63a9e
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-04-14 01:27:50 -0500

    Adds styling and completes comment post

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,24 +1,24 @@
 async function commentFormHandler(event) {
   event.preventDefault();
 
-  const body = document.querySelector("#commentBody").value.trim();
-  console.log(body);
+  const comment_body = document.querySelector("#commentInput").value.trim();
+
   const post_id = window.location.toString().split("/")[
     window.location.toString().split("/").length - 1
   ];
 
-  if (body && post_id) {
+  if (comment_body && post_id) {
     const response = await fetch("/api/comments", {
       method: "post",
       body: JSON.stringify({
-        body,
+        comment_body,
         post_id,
       }),
       headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
-      document.location.replace("/post:" + post_id);
+      document.location.replace("/post/" + post_id);
     } else {
       alert(response.statusText);
     }
