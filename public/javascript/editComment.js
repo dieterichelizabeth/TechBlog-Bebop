5e0c35663c93bb02c9cfe897b7e042ba728e176b
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-04-14 14:08:13 -0500

    adds styling to comment text area

--- public/javascript/editComment.js
+++ public/javascript/editComment.js
@@ -8,15 +8,11 @@ async function editCommentHandler(event) {
   console.log(commentEditor);
 
   const textarea = document.createElement("textarea");
-  textarea.classList.add("commentField");
+  textarea.classList.add("w100");
   textarea.innerHTML = previousComment;
 
   commentEditor.replaceWith(textarea);
-  // adds a dynamic "textarea" element w/ previous task description populated
-  // var commentEditor = $("<textarea>");
-  // $(this).replaceWith(changeTaskDescription);
   // pass the save button as an argument to the next function
-  // saveUpdate(forID, newTaskDescription);
 }
 
 // create a function to handle the routes to update the comment w/ event listener
