026c9918b539e2e1b2f9a45e35ce21a781dc4e99
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-04-14 02:04:11 -0500

    Completes delete button for post

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -31,6 +31,24 @@ async function editPostHandler(event) {
 
 async function deletePostHandler(event) {
   console.log("deleting post");
+
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+
+  const response = await fetch("/api/posts/" + id, {
+    method: "delete",
+    body: JSON.stringify({
+      id,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/homepage");
+  } else {
+    alert(response.statusText);
+  }
 }
 
 document
